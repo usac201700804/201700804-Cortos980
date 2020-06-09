0422c04cb5d5aa36d956062ec06a671891e656d8
dherreramorales <2872817520401@ingenieria.usac.edu.gt>
2020-06-09 12:22:50 -0600

    entrega

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -1,3 +1,24 @@
 #! /usr/bin/python3.8
+def par(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
 
-print('hello world')
+def collatz(N):
+    sec = [N]
+    while N != 1:
+        if par(N):
+            N //= 2
+        else:
+            N = 3*N + 1
+        sec.append(N)
+    return sec
+
+#mi numero de carnet es 201700804 por lo tanto mis secuencias generadas son de 2 a 804archivo = open('collatz.txt', 'w')
+archivo = open('collatz.txt', 'w')
+for i in range(2, 805):
+    archivo.write(str(str(collatz(i))+'\n'))
+    print(collatz(i))
+archivo.close()
+    
